04ef633e5e521ce43ef09a5c832db8e01e543e05
Parth Shekte <shekteparth1@gmail.com>
2026-03-22 17:13:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7741138113-1f6ac",
+  "appId": "1:269317219713:web:73cd12811737c46481f480",
+  "apiKey": "AIzaSyBso3HZEeDnYzz4FLJGSiwurVQIaJaIRSE",
+  "authDomain": "studio-7741138113-1f6ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "269317219713"
+};
