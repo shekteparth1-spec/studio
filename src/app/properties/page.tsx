55978452f1381c9eaf70b87846089c4ff2d3adcb
Parth Shekte <shekteparth1@gmail.com>
2026-03-23 04:26:58 +0000

    Try fixing this error: `Runtime ReferenceError: Can't find variable: Sep

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -26,6 +26,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import {
   Select,
   SelectContent,
